5c7b841a73442ea72f66a3cf2145b694cf4aa4e6
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-04-06 13:40:11 -0700

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+    <Routes>
+        {/* <Route
+            path='/'
+            element={}
+        />
+        <Route
+            path='/about'
+            element={<About />}
+        /> */}
+    </Routes>
+</Router>
+  );
+}
+
+export default App;
