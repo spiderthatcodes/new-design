8133d969e1c47f9d6886d0d7af10832e78e1bad3
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-04-07 08:42:27 -0700

    changes?

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,9 +4,7 @@ import Layout from '../../components/Layout/Layout';
 const Home = ({ isMobile }) => {
     return (
         <Layout isMobile={isMobile}>
-            <h1>One</h1>
-            <h2>Two</h2>
-            <h3>Three</h3>
+            <h1>Testing process</h1>
         </Layout>
     );
 };
