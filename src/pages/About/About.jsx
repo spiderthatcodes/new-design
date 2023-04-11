a4d31ea09d46e0a5e16b1a07238e5a1583333791
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-04-11 09:58:36 -0700

    fixed broken link

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,14 +7,39 @@ const About = ({ isMobile }) => {
             <Container isMobile={isMobile}>
                 <Paragraph isMobile={isMobile}>
                     My name is Jessica, and I am a full stack developer living
-                    in Phoenix, AZ. I am most comfortable working in the
-                    MERN stack, but I also have nearly 2 years of <a href='https://banyanlabs.io/indeed/'>professional
-                    experience</a> with Java and Springboot. I'm also a certified <a href='https://www.credly.com/badges/019e5ddf-eb1d-4287-a123-856bac6a13f2'>Professional Scrum Master</a>. I am always engaged in professional development; one reason I love programming is there is always something new to learn.
+                    in Phoenix, AZ. I am most comfortable working in the MERN
+                    stack, but I also have nearly 2 years of{' '}
+                    <a
+                        href='https://banyanlabs.io/indeed/'
+                        target='blank'
+                    >
+                        professional experience
+                    </a>{' '}
+                    with Java and Springboot. I'm also a certified{' '}
+                    <a
+                        href='https://www.credly.com/earner/earned/badge/b38a24b9-7aae-480a-88f8-28bd6c69e960'
+                        target='blank'
+                    >
+                        Professional Scrum Master
+                    </a>
+                    . I am always engaged in professional development; the
+                    ability to engage in lifelong learning is one reason I love
+                    to code.
                 </Paragraph>
                 <Paragraph isMobile={isMobile}>
                     I currently teach justice involved individuals to code for a
                     non-profit called{' '}
-                    <a href='https://www.perseverenow.org/'>Persevere</a>, as well as working freelance as a web developer. I am grateful that I have the opportunity to do the two things I love at the same time: teach and code. Every day I know that my work genuinely makes a difference in the lives of others, and that's pretty awesome.
+                    <a
+                        href='https://www.perseverenow.org/'
+                        target='blank'
+                    >
+                        Persevere
+                    </a>
+                    , as well as working freelance as a web developer. I am
+                    grateful that I have the opportunity to do the two things I
+                    love at the same time: teach and code. Every day I know that
+                    my work genuinely makes a difference in the lives of others,
+                    and that's pretty awesome.
                 </Paragraph>
             </Container>
         </Layout>
